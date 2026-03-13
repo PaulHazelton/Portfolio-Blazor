17dba9892e91fc5200f8664690b8990eeeae1b3c
Paul Hazelton <hazeltonspaul@gmail.com>
2026-03-12 20:51:18 -0400

    Added exported html files

--- Portfolio/Pages/ProgrammingBlogs/Blog.razor.cs
+++ Portfolio/Pages/ProgrammingBlogs/Blog.razor.cs
@@ -8,13 +8,10 @@ public partial class Blog
 	[Parameter]
 	public required string FileName { get; set; }
 
-	// private string? _markdown;
 	private string? _blogMarkup;
 
 	protected override async Task OnInitializedAsync()
 	{
-		var markdown = await HttpClient.GetStringAsync($"documents/blogs/{FileName}.md");
-
-		// _blogMarkup = Markdown.ToHtml(markdown);
+		_blogMarkup = await HttpClient.GetStringAsync($"documents/blogs/{FileName}.html");
 	}
 }
