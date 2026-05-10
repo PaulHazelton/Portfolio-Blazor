eeb3e8ffc7af684147bfb29784611ec0a4a3e80f
Paul Hazelton <hazeltonspaul@gmail.com>
2026-05-09 23:33:54 -0400

    Added zoom notify project preview

--- Portfolio/Routes.cs
+++ Portfolio/Routes.cs
@@ -12,6 +12,7 @@ public static class Routes
 	public static class Projects
 	{
 		public const string Recoil = $"{ProjectList}/recoil";
+		public const string ZoomNotify = $"{ProjectList}/zoom-notify";
 		public const string CpuSim = $"{ProjectList}/cpu-sim";
 		public const string MonoGameResponsiveGui = $"{ProjectList}/monogame-responsive-gui";
 		public const string Tilteroids = $"{ProjectList}/tilteroids";
